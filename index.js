e35f12fb7325776e38ec740f4e294b06c6682f3d
DanaOrr <danaaloni5@gmail.com>
2020-12-01 17:39:57 +0200

    dana

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.json({response: "index route"}));
-app.get('/playlist', (req,res) => res.send("playlist page"));
+app.get('/playlist', (req,res) => res.send("playlist page")); //Yarin
 app.put('/language', (req,res) => res.send("language")); //chen 
 app.delete('/playlist', (req,res) => res.send("Playlist Deleted")); //Omri
+app.get('/songs', (req,res) => res.send("Songs page")); // Dana
 
 /*  Every team member has to add a route to this file, like the one above
     For example: 
