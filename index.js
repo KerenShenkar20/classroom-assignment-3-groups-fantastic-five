6ebbbd1a2ec88ab7f06aa889b3242b3a56f24574
Omri93h <Omri93h@gmail.com>
2020-12-01 17:08:43 +0200

    Adding delete method

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => res.json({response: "index route"}));
 app.get('/playlist', (req,res) => res.send("playlist page"));
 app.put('/language', (req,res) => res.send("language")); //chen 
+app.delete('/playlist', (req,res) => res.send("Playlist Deleted")); //Omri
 
 /*  Every team member has to add a route to this file, like the one above
     For example: 
