4188bda5b9d6a1d8ec259fa04c9306a259436220
Chen <chen.nahoom@gmail.com>
2020-12-01 13:54:05 +0200

    Chen - put route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.json({response: "index route"}));
 app.get('/playlist', (req,res) => res.send("playlist page"));
+app.put('/language', (req,res) => res.send("language")); //chen 
 
 /*  Every team member has to add a route to this file, like the one above
     For example: 
