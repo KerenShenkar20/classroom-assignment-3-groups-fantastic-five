e865931143d46e813980694f8d389725d55f4059
saarp <saarp@liveperson.com>
2020-12-01 18:22:27 +0200

    Saar's commit

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get('/playlist', (req,res) => res.send("playlist page")); //Yarin
 app.put('/language', (req,res) => res.send("language")); //chen 
 app.delete('/playlist', (req,res) => res.send("Playlist Deleted")); //Omri
 app.get('/songs', (req,res) => res.send("Songs page")); // Dana
+app.post('/login',(req, res) => {
+    var user_name = req.body.user;
+    var password = req.body.password;
+    console.log("User name = "+user_name+", password is "+password);
+    res.send("Loged in")
+}); // Saar Pernik
+
 
 /*  Every team member has to add a route to this file, like the one above
     For example: 
